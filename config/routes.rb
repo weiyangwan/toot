22f4b30659a5b7cef278ec57a71901aff99eb4c5
weiyang wan <weiyangwan@gmail.com>
2016-08-01 01:00:47 +0800

    Added user with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  get '/about',  to: 'static_pages#about'
+  get '/help',   to: 'static_pages#help'
 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
