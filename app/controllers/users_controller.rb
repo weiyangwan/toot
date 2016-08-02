62fca900ac9f1d60b92e0330f2d3263d65268db5
weiyang wan <weiyangwan@gmail.com>
2016-08-02 18:21:24 +0800

    Populated the followers/following list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:show]
+  # before_action :correct_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
