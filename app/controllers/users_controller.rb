636a6ce237357704c70ddc410bb40c95ba138205
weiyang wan <weiyangwan@gmail.com>
2016-08-02 14:49:07 +0800

    Prevented user to access another user page and added flash partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page])
     @post = @user.posts.build
   end
+
+  private
+
+    def current_user?(user)
+      user == current_user
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        flash[:warning] = "You are not authorised to access the page you requested."
+        redirect_to current_user
+      end
+    end
+
 
 end
