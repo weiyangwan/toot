66365545691afda0066b6456c32e69d920e9d1f8
weiyang wan <weiyangwan@gmail.com>
2016-08-03 17:20:40 +0800

    Committing before migrate to include group_id in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :group
+
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence:true
