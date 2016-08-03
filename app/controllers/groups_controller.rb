4795e3c7d18b9b5a4713f966f1967695613d975e
weiyang wan <weiyangwan@gmail.com>
2016-08-03 11:39:22 +0800

    Added group controller and created group_form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      flash[:success] = "You have created a new group. Add your friends to start a conversation!"
+      #need to redirect to group/Url
+    else
+      flash[:danger] = "Something went wrong when creating a group. Please try again."
+      redirect_to user_path(current_user)
+  end
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name)
+    end
+
 end
