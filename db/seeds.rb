d27fcf244049d79307bf254c8436152b6186fe79
weiyang wan <weiyangwan@gmail.com>
2016-08-04 17:26:42 +0800

    Updated to merge to master

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,10 +31,9 @@ User.create!([
               password_confirmation: password)
 end
 
-
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..20]
+followers = users[30..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
