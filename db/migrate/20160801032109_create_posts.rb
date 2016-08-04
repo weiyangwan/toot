be18664a3fd9479df77ebfc9885e366f65a02115
weiyang wan <weiyangwan@gmail.com>
2016-08-04 17:54:15 +0800

    Edit Post migration file to exclude foreign key for group

--- db/migrate/20160801032109_create_posts.rb
+++ db/migrate/20160801032109_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     create_table :posts do |t|
       t.text :content
       t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      t.references :group
 
       t.timestamps
     end
